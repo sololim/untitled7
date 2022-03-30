5d4a38c31e24d7dc6c5b55a0dbed6b95452f2fb2
sololim <101944276+sololim@users.noreply.github.com>
2022-03-30 16:23:06 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+basic.forever(function () {
+    if (input.soundLevel() == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (input.soundLevel() == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    }
+    if (input.soundLevel() == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.soundLevel() == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.soundLevel() == 4) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.soundLevel() == 5) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
